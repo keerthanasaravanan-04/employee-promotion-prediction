df06fa6e0d1d50198641c416a67fcceb21ac6b08
keerthana saravanan <keerthanasaravanan008@gmail.com>
2024-09-12 06:26:27 -0700

    Add files via upload

---
+++ SOURCE CODE/Assets/employee_pred.py
@@ -0,0 +1,21 @@
+
+import pickle
+
+# de-serialize model.pkl file into an object called mdl using pickle
+# with open('Assets\pickle\model.pkl', 'rb') as handle:
+with open(r'D:\Arun\new developement\new ongoing Development\EMPLOYEE PROMOTION PREDICTION\SOURCE CODE/Assets/pickle/model.pkl', 'rb') as handle:
+    mdl = pickle.load(handle)
+
+# de-serialize scalar.pkl file into an object called scl using pickle
+# with open('Assets\pickle\scalar.pkl', 'rb') as handle:
+with open(r'D:\Arun\new developement\new ongoing Development\EMPLOYEE PROMOTION PREDICTION\SOURCE CODE/Assets/pickle/scalar.pkl', 'rb') as handle:
+    scl = pickle.load(handle)
+
+# scale the user fed data
+def scale(sample):
+    return scl.transform(sample)
+
+# make predicion
+def predict(sample):
+    sample_scaled = scale(sample)
+    return mdl.predict(sample_scaled)
